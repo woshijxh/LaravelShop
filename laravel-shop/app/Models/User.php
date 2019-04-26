b7d0bc0d7edd643054629afa2a99b3a9a46934c1
jxh <1059751314@qq.com>
2019-04-26 09:40:32 +0800

    邮箱验证功能

--- laravel-shop/app/Models/User.php
+++ laravel-shop/app/Models/User.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App;
+namespace App\Models;
 
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
@@ -37,3 +37,4 @@ class User extends Authenticatable
         'email_verified_at' => 'datetime',
     ];
 }
+
--- laravel-shop/routes/web.php
+++ laravel-shop/routes/web.php
@@ -14,5 +14,6 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', 'PagesController@root')->name('root');
 
-Auth::routes();
+Auth::routes(['verify' => true]);
+
 
